f3eeabee2f12c1f7df915c8ead01cad368592bf9
radwa <radwaalafandy4@gmail.com>
2024-11-30 02:43:33 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
-// lib/main.dart
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_app/auth/login.dart';
 import 'package:store_app/auth/signup.dart';
+import 'package:store_app/screens/Verified%20_Email.dart';
 import 'package:store_app/screens/admin_page.dart';
 import 'package:store_app/screens/cart_screen.dart';
 import 'package:store_app/screens/category_app.dart';
-import 'package:store_app/screens/Verified _Email.dart';
-import 'package:store_app/screens/homeees.dart';
 
+import 'package:store_app/screens/homeees.dart';
 import 'package:store_app/screens/onboarding_screen.dart';
 import 'package:store_app/screens/product_detailed_screen.dart';
 import 'package:store_app/screens/splash_screen.dart';
 import 'package:store_app/screens/update_product_page.dart';
-
+import 'package:device_preview/device_preview.dart'; // Add this import
+import 'package:flutter/foundation.dart'; // For kReleaseMode
+import 'package:store_app/screens/Verified _Email.dart';
 import 'cart_provider.dart';
 import 'models/product_model.dart';
 
@@ -25,13 +25,20 @@ import 'models/product_model.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: FirebaseOptions(
-          apiKey: 'AIzaSyAoGcREMMYU0j7ZGY9eJT38a8lZkAZDC70',
-          appId: '1:849992411206:android:40e7eeaf7f004d72b38247',
-          messagingSenderId: '849992411206',
-          projectId: 'storeeeeeeeeee-1fb10'));
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyAoGcREMMYU0j7ZGY9eJT38a8lZkAZDC70',
+      appId: '1:849992411206:android:40e7eeaf7f004d72b38247',
+      messagingSenderId: '849992411206',
+      projectId: 'storeeeeeeeeee-1fb10',
+    ),
+  );
 
-  runApp(StoreApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode, // Enable in debug mode only
+      builder: (context) => StoreApp(), // Wrap StoreApp with DevicePreview
+    ),
+  );
 }
 
 class StoreApp extends StatefulWidget {
@@ -57,30 +64,30 @@ class _MyAppState extends State<StoreApp> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-        create: (context) => CartProvider(),
-    child:  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: (FirebaseAuth.instance.currentUser != null &&
-              FirebaseAuth.instance.currentUser!.emailVerified)
-          ? HomePage()
-          : SplashScreen(),
-      routes: {
-        "signup": (context) => SignUp(),
-        "login": (context) => Login(),
-        "HomeScreen": (context) => HomeScreen(),
-        "HomeApp": (context)=> HomeApp(),
-        "AdminPage": (context) => AdminPage(),
-        "onboard_screen": (context) => OnBoard(),
-        ProductDetailsPage.id: (context) => ProductDetailsPage(
-          product:
-          ModalRoute.of(context)!.settings.arguments as ProductModel,
-        ),
-        UpdateProductPage.id: (context) => UpdateProductPage(),
-        CartScreen.id: (context) => CartScreen(),
-
-      },
-
-    ),
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        builder: DevicePreview.appBuilder, // Add DevicePreview.builder here
+        useInheritedMediaQuery: true, // To adapt media queries from the device preview
+        locale: DevicePreview.locale(context), // To simulate locales in preview
+        home: (FirebaseAuth.instance.currentUser != null &&
+            FirebaseAuth.instance.currentUser!.emailVerified)
+            ? HomePage()
+            : SplashScreen(),
+        routes: {
+          "signup": (context) => SignUp(),
+          "login": (context) => Login(),
+          "HomeScreen": (context) => HomeScreen(),
+          "HomeApp": (context) => HomeApp(),
+          "AdminPage": (context) => AdminPage(),
+          "onboard_screen": (context) => OnBoard(),
+          ProductDetailsPage.id: (context) => ProductDetailsPage(
+            product: ModalRoute.of(context)!.settings.arguments as ProductModel,
+          ),
+          UpdateProductPage.id: (context) => UpdateProductPage(),
+          CartScreen.id: (context) => CartScreen(),
+        },
+      ),
     );
   }
 }
